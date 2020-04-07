517781dfa4149402f855a6690da0e35fae57c047
Jossie Esteban Rodríguez Salazar <cto@losmoviles.com.co>
2020-04-07 14:17:44 -0500

    add http provider constructor into wire set

--- pkg/http/wire.go
+++ pkg/http/wire.go
@@ -5,6 +5,8 @@ import "github.com/google/wire"
 var Set = wire.NewSet(
 	NewResponder,
 	NewServer,
+	NewProvider,
 	wire.Bind(new(Responder), new(*responder)),
 	wire.Bind(new(Server), new(*server)),
+	wire.Bind(new(Provider), new(*provider)),
 )
